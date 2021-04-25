53fe8c5c5536ddced817efa68e4ba8a54440b6c0
moderneinstein <harsh.srivastva@rediffmail.com>
2021-04-25 14:56:24 +0530

    improved code formatting

--- index.js
+++ index.js
@@ -1,55 +1,52 @@
-function attendance(conducted, absent){
-	var percent = (conducted-absent)/conducted*100;
+function attendance(conducted, absent) {
+	var percent = (conducted - absent) / conducted * 100;
 	return percent;
 }
-function check(){
+
+function check() {
 	var conducted = document.forms["form"]["conducted"].value;
 	var absent = document.forms["form"]["absent"].value;
-	if(conducted == ""){
+	if (conducted == "") {
 		alert("Enter hours conducted");
 		document.form.conducted.focus();
 		return false;
 	}
-	if(absent == ""){
+	if (absent == "") {
 		alert("Enter hours absent");
-		document.form.absent.focus() ;
+		document.form.absent.focus();
 		return false;
 	}
-	if(parseInt(absent) > parseInt(conducted)){
+	if (parseInt(absent) > parseInt(conducted)) {
 		alert("Number of hours absent cannot be greater than hours conducted.")
 		document.form.absent.focus();
 		return false;
 	}
 	var percent = attendance(conducted, absent);
-	if(percent < 75)
-		var to_attend = absent*4 - conducted;
+	if (percent < 75)
+		var to_attend = absent * 4 - conducted;
 	var current = percent;
-	current = Math.round(current*100) / 100;
+	current = Math.round(current * 100) / 100;
 	var count = 0;
-	while(percent > 75){
-		if(attendance(conducted+1, absent+1) < 75)	break;
+	while (percent > 75) {
+		if (attendance(conducted + 1, absent + 1) < 75) break;
 		count++;
 		absent++;
 		conducted++;
 		percent = attendance(conducted, absent);
 	}
 	document.querySelector('.showResult').innerHTML = "Current Attendance = " + current + "%";
-	if(count > 1){	
+	if (count > 1) {
 		document.querySelector('.showDescription').innerHTML = "You can miss " + count + " class.";
 
-	}
-	else if(count == 1)
-	{
+	} else if (count == 1) {
 		document.querySelector('.showDescription').innerHTML = "You can miss " + count + " class.";
 
-	}
-	else if(current < 75){
+	} else if (current < 75) {
 		document.querySelector('.showDescription').innerHTML = "You cannot miss any class.\n" + "You need to attend " + to_attend + " more classes for 75% attendance.";
 
-	}
-	else{	
+	} else {
 		document.querySelector('.showDescription').innerHTML = "You cannot miss any class.";
 	}
-	
-		return false;
+
+	return false;
 }
